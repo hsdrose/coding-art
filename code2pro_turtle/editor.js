2d738298f82baa7adac48ab8844e6226b8183d09
Viet Le <vietlq85@gmail.com>
2019-05-02 08:17:49 +0700

    Hilbert Fractal Maze

--- code2pro_turtle/editor.js
+++ code2pro_turtle/editor.js
@@ -313,4 +313,27 @@ for x in range(180):
     turtle.rt(2)
 `;
 
+    window.art_works.art_fractal_maze = `# Fractal maze
+# Source: https://blog.klipse.tech/python/2017/01/04/python-turtle-fractal.html
+# loaded from gist: https://gist.github.com/viebel/5349bcca144c41b8f83af39079bf59ad
+
+import turtle
+
+def hilbert2(step, rule, angle, depth, t):
+    if depth > 0:
+        a = lambda: hilbert2(step, "a", angle, depth - 1, t)
+        b = lambda: hilbert2(step, "b", angle, depth - 1, t)
+        left = lambda: t.left(angle)
+        right = lambda: t.right(angle)
+        forward = lambda: t.forward(step)
+        if rule == "a":
+            left(); b(); forward(); right(); a(); forward(); a(); right(); forward(); b(); left();
+        if rule == "b":
+            right(); a(); forward(); left(); b(); forward(); b(); left(); forward(); a(); right();
+
+myTurtle = turtle.Turtle()
+myTurtle.speed(0)
+hilbert2(5, "a", 90, 5, myTurtle)
+`;
+
 }
